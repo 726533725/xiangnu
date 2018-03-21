d74c372d4065cbc4774645948e042f4f7838e192
726533725 <726533725@qq.com>
2018-03-21 18:34:14 +0800

    222

---
+++ donly/js/index.js
@@ -0,0 +1,11 @@
+//二级菜单
+var  nav =document.getElementById("tuomuse")
+  var 	submuse =document.getElementById("submuse")
+	nav.onmouseover=function(){
+		submuse.style.display = "block"
+	}
+	nav.onmouseout=function(){
+		submuse.style.display = "none"
+	}
+//选项卡
+ 
